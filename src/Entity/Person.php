8d02b6de3b7b82f8497369690a86aa0e025cf3c1
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2019-09-17 10:50:47 +0200

    ✨ [Auth] Add user

---
+++ src/Entity/Person.php
@@ -0,0 +1,80 @@
+<?php
+// api/src/Entity/Book.php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A book.
+ *
+ * @ORM\Entity
+ * @ApiResource
+ *
+ */
+class Person
+{
+    /**
+     * @var int The id of this person.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string.
+     *
+     * @ORM\Column(name="first_name")
+     */
+    public $firstName;
+
+    /**
+     * @var string.
+     *
+     * @ORM\Column(name="last_name")
+     */
+    public $lastName;
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
