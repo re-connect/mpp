47fde077fba92bdd0fef9123fa728d99616819ea
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2019-09-20 09:58:11 +0200

    📦 [Add] Profiler pack

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
