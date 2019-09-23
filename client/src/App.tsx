50c45dddfbdcd16a3679d042232153248a7cd2ab
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2019-09-23 19:32:51 +0200

    ✨ [Tooling] Fix all env problems

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,24 @@
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import * as React from 'react';
 import Routes from './Routes';
 
-const App = () => <Routes />;
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#54D69E',
+      main: '#1ABD9C',
+      dark: '#148F76',
+      contrastText: '#fff',
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Routes />
+  </ThemeProvider>
+);
 
 export default App;
--- client/src/Services/requests.tsx
+++ client/src/Services/requests.tsx
@@ -1,4 +1,4 @@
-export const backendUrl = 'http://155.133.130.39:82';
+export const backendUrl = process.env.NODE_ENV === 'production' ? 'http://155.133.130.39:82' : 'http://localhost:8000';
 export const apiEndpoint = `${backendUrl}/api`;
 
 export const userEndpoint = `${apiEndpoint}/people`;
