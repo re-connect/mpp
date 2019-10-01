9fdd3f016b0b8c584352cdb392b7819d16d37870
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2019-10-01 09:34:10 +0200

    🔐 [Api] Change api url to https and dns

--- client/src/Services/requests.tsx
+++ client/src/Services/requests.tsx
@@ -1,4 +1,5 @@
-export const backendUrl = process.env.NODE_ENV === 'production' ? 'http://155.133.130.39:82' : 'http://localhost:8000';
+export const backendUrl =
+  process.env.NODE_ENV === 'production' ? 'https://api.mpm.reconnect.fr' : 'http://localhost:8000';
 export const apiEndpoint = `${backendUrl}/api`;
 
 export const userEndpoint = `${apiEndpoint}/people`;
