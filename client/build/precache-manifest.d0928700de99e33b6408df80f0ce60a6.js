9fa6eeca018cd9ed90dd441c58d6043b6eda11ac
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2021-07-29 16:20:10 +0200

    🍱 [Build] Add all assets

---
+++ client/build/precache-manifest.d0928700de99e33b6408df80f0ce60a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b637838518655683fb25f4023a0e071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce82515f13216cfa372b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39897d6fddf7bd303fbf",
+    "url": "/static/js/2.bccb7363.chunk.js"
+  },
+  {
+    "revision": "ce82515f13216cfa372b",
+    "url": "/static/js/main.085b9787.chunk.js"
+  },
+  {
+    "revision": "2d5ec2dd335a7752c6ed",
+    "url": "/static/js/runtime~main.d6df33e4.js"
+  },
+  {
+    "revision": "2054d5e328212144b318c9f9fcabf417",
+    "url": "/static/media/logo.2054d5e3.png"
+  }
+]);
