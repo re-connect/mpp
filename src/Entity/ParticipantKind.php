10967252089928c2e17b633c5e0a2b1ec5b7f74e
Your Name <you@example.com>
2021-11-05 10:21:36 +0100

    :sparkles: [ParticipantKind] Create entity

---
+++ src/Entity/ParticipantKind.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ParticipantKindRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ParticipantKindRepository::class)
+ */
+class ParticipantKind
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
