e000326640e28db5b630489dca82aa672e052e6a
Your Name <you@example.com>
2021-11-16 14:27:43 +0100

    :sparkles: [Workshops] Display workshops list from a center

--- client/src/Pages/Workshops/Workshops.tsx
+++ client/src/Pages/Workshops/Workshops.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import useFetchCenter from '../../Services/useFetchCenter';
+import WorkshopsContext from '../../Context/WorkshopsContext';
+import useFetchActivities from '../../Services/useFetchActivities';
 
 const Workshops = () => {
     const center = useFetchCenter();
+    const workshopsContext = useContext(WorkshopsContext);
+    useFetchActivities();
 
     return (
         <div>
             <h1>{center.name}</h1>
             <h2>Nombre d'ateliers : {center.workshops.length}</h2>
+            <ol>
+                {workshopsContext.list.map((workshop: any) => (
+                    <li key={workshop.id}>Atelier n° {workshop.id}</li>
+                ))}
+            </ol>
         </div>
     );
 };
