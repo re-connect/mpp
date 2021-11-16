f4f83b7e4ade2709f122facb2934c24b8edc879c
Your Name <you@example.com>
2021-11-16 11:58:34 +0100

    :sparkles: [useFetchActivities] Start a common hook in order to fetch perms or workshops based on page url

---
+++ client/src/Services/useFetchActivities.tsx
@@ -0,0 +1,37 @@
+import {useCallback, useContext, useEffect} from 'react';
+import {notesEndpoint, workshopsEndpoint} from './requests';
+import {useHistory, useParams} from "react-router-dom";
+import NotesContext from '../Context/NotesContext';
+import WorkshopsContext from '../Context/WorkshopsContext';
+import axios from 'axios';
+
+function useFetchActivities() {
+    const notesContext = useContext(NotesContext);
+    const workshopsContext = useContext(WorkshopsContext);
+    const centerId = useParams().centerId;
+    const history = useHistory();
+    const pathName = history.location.pathname;
+    const endpoint = pathName.includes('workshops') ? workshopsEndpoint : notesEndpoint;
+    const activitiesContext = pathName.includes('workshops') ? workshopsContext : notesContext;
+
+    const fetchActivities = useCallback(() => {
+        const token = localStorage.getItem('token');
+        if (token !== null) {
+            axios
+                .get(`${endpoint}?center=${centerId}`, {
+                    headers: {Authorization: `Bearer ${token}`}
+                })
+                .then((response) => {
+                    activitiesContext.set(response.data['hydra:member']);
+                });
+        } else {
+            history.push('/login');
+        }
+    }, [centerId, history]);
+
+    useEffect(() => {
+        fetchActivities();
+    }, [fetchActivities, centerId]);
+}
+
+export default useFetchActivities;
