f90fbd665a87dd9e0ab39cf5438acd736726e29f
Your Name <you@example.com>
2021-11-17 11:49:18 +0100

    :recycle: [useFetchWorkshops] Refractor code

--- client/src/Services/useFetchWorkshops.tsx
+++ client/src/Services/useFetchWorkshops.tsx
@@ -1,27 +1,23 @@
 import {useCallback, useContext, useEffect} from 'react';
-import {notesEndpoint, workshopsEndpoint} from './requests';
+import {workshopsEndpoint} from './requests';
 import {useHistory, useParams} from 'react-router-dom';
-import NotesContext from '../Context/NotesContext';
 import WorkshopsContext from '../Context/WorkshopsContext';
 import axios from 'axios';
 
 function useFetchWorkshops() {
-    const notesContext = useContext(NotesContext);
-    const workshopsContext = useContext(WorkshopsContext);
-    const centerId = useParams().centerId;
+    const {setWorkshops} = useContext(WorkshopsContext);
     const history = useHistory();
-    const pathName = history.location.pathname;
-    const endpoint = pathName.includes('workshops') ? workshopsEndpoint : notesEndpoint;
-    const activitiesContext = pathName.includes('workshops') ? workshopsContext : notesContext;
+    const centerId = useParams().centerId;
 
     const fetchWorkshops = useCallback((centerId) => {
         const token = localStorage.getItem('token');
         if (token !== null) {
             axios
-                .get(`${endpoint}?center=${centerId}`, {
+                .get(`${workshopsEndpoint}?center=${centerId}`, {
                     headers: {Authorization: `Bearer ${token}`}
                 })
                 .then((response) => {
+                    setWorkshops(response.data['hydra:member']);
                 });
         } else {
             history.push('/login');
