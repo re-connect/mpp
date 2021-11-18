fe98fbeb3639487ab01ca7267e7ce04b754f05b5
Your Name <you@example.com>
2021-11-18 12:34:45 +0100

    :sparkles: [Skill] Add normalization groups

--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -7,9 +7,12 @@ use App\Repository\SkillRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}}
+ * )
  * @ORM\Entity(repositoryClass=SkillRepository::class)
  */
 class Skill
@@ -23,6 +26,7 @@ class Skill
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"})
      */
     private ?string $name;
 
