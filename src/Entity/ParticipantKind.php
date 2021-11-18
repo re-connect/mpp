1e2eb1cd333005a85df28c26ec9e57b6b5c68702
Your Name <you@example.com>
2021-11-18 17:43:40 +0100

    :sparkles: [Entities] Add toString function for admin dashboard rendering

--- src/Entity/ParticipantKind.php
+++ src/Entity/ParticipantKind.php
@@ -40,6 +40,11 @@ class ParticipantKind
         $this->workshops = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
--- src/Entity/Topic.php
+++ src/Entity/Topic.php
@@ -40,6 +40,11 @@ class Topic
         $this->workshops = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
