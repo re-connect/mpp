efdbf206965eeaa5d5fe91b686fbc650441b15d3
Your Name <you@example.com>
2021-11-19 15:02:50 +0100

    :sparkles: [TopicsContext] Add provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,38 +6,43 @@ import Routes from './Routes';
 import {Note} from './Types/Notes';
 import {Workshop} from './Types/Workshops';
 import WorkshopsContext from './Context/WorkshopsContext';
+import {Topic} from "./Types/Topics";
+import TopicsContext from "./Context/TopicsContext";
 
 const theme = createTheme({
-  palette: {
-    type: 'dark',
-    background: {
-      default: '#28ad7a',
-      paper: '#28ad7a',
+    palette: {
+        type: 'dark',
+        background: {
+            default: '#28ad7a',
+            paper: '#28ad7a',
+        },
+        text: {
+            primary: '#eff1f7',
+            secondary: '#eff1f7',
+        },
+        primary: {
+            main: '#eff1f7',
+            contrastText: '#677273',
+        },
     },
-    text: {
-      primary: '#eff1f7',
-      secondary: '#eff1f7',
-    },
-    primary: {
-      main: '#eff1f7',
-      contrastText: '#677273',
-    },
-  },
 });
 
 const App = () => {
-  const [notes, setNotes] = React.useState<Note[]>([]);
-  const [workshops, setWorkshops] = React.useState<Workshop[]>([]);
+    const [notes, setNotes] = React.useState<Note[]>([]);
+    const [workshops, setWorkshops] = React.useState<Workshop[]>([]);
+    const [topics, setTopics] = React.useState<Topic[]>([]);
 
-  return (
-    <ThemeProvider theme={theme}>
-          <WorkshopsContext.Provider value={{list: workshops, setWorkshops: setWorkshops}}>
-              <NotesContext.Provider value={{list: notes, set: setNotes}}>
-                  <Routes/>
-              </NotesContext.Provider>
-          </WorkshopsContext.Provider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <WorkshopsContext.Provider value={{list: workshops, setWorkshops: setWorkshops}}>
+                <NotesContext.Provider value={{list: notes, set: setNotes}}>
+                    <TopicsContext.Provider value={{listTopics: topics, setTopics: setTopics}}>
+                        <Routes/>
+                    </TopicsContext.Provider>
+                </NotesContext.Provider>
+            </WorkshopsContext.Provider>
+        </ThemeProvider>
+    );
 };
 
 export default App;
