87adeddc2b9150b66549b3916239109e21c866de
Your Name <you@example.com>
2021-11-19 15:26:03 +0100

    :sparkles: [CreateWorkshopModal] Fetch topics and render a list

--- client/src/Pages/Workshops/Components/CreateWorkshopForm.tsx
+++ client/src/Pages/Workshops/Components/CreateWorkshopForm.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import axios from 'axios';
+import {topicsEndpoint} from '../../../Services/requests';
+import TopicsContext from '../../../Context/TopicsContext';
+import {useHistory} from 'react-router-dom';
 
 const CreateWorkshopForm = () => {
+    const topicsContext = useContext(TopicsContext);
+    const history = useHistory();
+
+    if (topicsContext.listTopics.length === 0) {
+        const token = localStorage.getItem('token');
+        if (token !== null) {
+            axios
+                .get(`${topicsEndpoint}`, {
+                    headers: {Authorization: `Bearer ${token}`}
+                })
+                .then((response) => {
+                    topicsContext.setTopics(response.data['hydra:member']);
+                    console.log(response.data['hydra:member'])
+                });
+        } else {
+            history.push('/login');
+        }
+    }
+
     return (
-        <div>I'm a modal</div>
+        <div>
+            <div>Topics list</div>
+            <ul>
+                {topicsContext.listTopics.map((topic: any) => (
+                    <li>{topic.name}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
