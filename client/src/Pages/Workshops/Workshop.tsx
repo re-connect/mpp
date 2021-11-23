e421853d0a507b27978308c3de5e17e24a8f1cb6
Your Name <you@example.com>
2021-11-23 11:34:49 +0100

    :art: [Workshop] Fix indentation

--- client/src/Pages/Workshops/Workshop.tsx
+++ client/src/Pages/Workshops/Workshop.tsx
@@ -8,18 +8,18 @@ const StyledCard = styled(Card)`
 `;
 
 const Workshop = ({workshop}: any) => {
-    return (
-      <StyledCard key={workshop.id}>
-          <CardContent style={{position: 'relative', backgroundColor: '#212121'}}>
-              <Typography color='textPrimary' gutterBottom>
-                  Date : {format(new Date(workshop.date), 'dd-MM-yyyy')}
-              </Typography>
-              <Typography variant='body2' component='p'>
-                  Nombre de participants : {workshop.nbParticipants}
-              </Typography>
-          </CardContent>
-      </StyledCard>
-    );
+  return (
+    <StyledCard key={workshop.id}>
+      <CardContent style={{position: 'relative', backgroundColor: '#212121'}}>
+        <Typography color='textPrimary' gutterBottom>
+          Date : {format(new Date(workshop.date), 'dd-MM-yyyy')}
+        </Typography>
+        <Typography variant='body2' component='p'>
+          Nombre de participants : {workshop.nbParticipants}
+        </Typography>
+      </CardContent>
+    </StyledCard>
+  );
 };
 
 export default Workshop;
