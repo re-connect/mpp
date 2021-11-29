497ac699c00a02dca8123f5223ecbb4ca5e5063d
Your Name <you@example.com>
2021-11-29 15:18:00 +0100

    :sparkles: [build] Yarn build

---
+++ client/build/precache-manifest.3079a7e5a6d0fd89fc6754793cdb25c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de6a7b6d16bdde13caae03f784f670b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866c4c67aeafd1900741",
+    "url": "/static/css/main.1ff7142d.chunk.css"
+  },
+  {
+    "revision": "0ebcf6b76cb26bd8a5fc",
+    "url": "/static/js/2.a584f812.chunk.js"
+  },
+  {
+    "revision": "866c4c67aeafd1900741",
+    "url": "/static/js/main.d95f49e9.chunk.js"
+  },
+  {
+    "revision": "2d5ec2dd335a7752c6ed",
+    "url": "/static/js/runtime~main.d6df33e4.js"
+  },
+  {
+    "revision": "2054d5e328212144b318c9f9fcabf417",
+    "url": "/static/media/logo.2054d5e3.png"
+  }
+]);
