f0d6c7c0611e318b2b964b3331a60187da6ff51b
Your Name <you@example.com>
2021-11-29 12:11:15 +0100

    :sparkles: [Workshops] Cards are ordered by date now

--- src/Entity/Workshop.php
+++ src/Entity/Workshop.php
@@ -14,6 +14,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
+ *     attributes={"order"={"date": "DESC"}},
  *     normalizationContext={"groups"={"read"}},
  *     denormalizationContext={"groups"={"write"}}
  * )
