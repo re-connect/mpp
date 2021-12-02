f9f3ed213172c9daea8a362570df2499e6035140
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2021-12-02 15:29:35 +0100

    ✨ [Dropdows] Add route to get all dropdowns

---
+++ src/Controller/DropdownsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Core\Api\IriConverterInterface;
+use App\Repository\AgeBreakpointRepository;
+use App\Repository\EquipmentSupplierRepository;
+use App\Repository\ParticipantKindRepository;
+use App\Repository\SkillRepository;
+use App\Repository\TopicRepository;
+use App\Repository\UsedEquipmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DropdownsController extends AbstractController
+{
+    /**
+     * @Route("/api/dropdowns", name="dropdowns")
+     */
+    public function index(
+        IriConverterInterface       $iriGenerator,
+        ParticipantKindRepository   $participantKindRepository,
+        EquipmentSupplierRepository $equipmentSupplierRepository,
+        AgeBreakpointRepository     $ageBreakpointRepository,
+        UsedEquipmentRepository     $usedEquipmentRepository,
+        TopicRepository             $topicRepository,
+        SkillRepository             $skillRepository
+    ): Response {
+        $dropdowns = [];
+        $repositories = [
+            'participantKinds' => $participantKindRepository,
+            'topics' => $topicRepository,
+            'equipmentSuppliers' => $equipmentSupplierRepository,
+            'ageBreakpoints' => $ageBreakpointRepository,
+            'usedEquipments' => $usedEquipmentRepository,
+            'skills' => $skillRepository,
+        ];
+
+        foreach ($repositories as $key => $repository) {
+            $values = [];
+            foreach ($repository->findAll() as $item) {
+                $values[$iriGenerator->getIriFromItem($item)] = $item;
+            }
+            $dropdowns[$key] = $values;
+        }
+
+        return $this->json($dropdowns);
+    }
+}
