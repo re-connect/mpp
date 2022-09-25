9eef5c175ebf65cb943b4f71d166626cc81fa132
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-09-25 16:14:25 +0200

    :sparkles: [Login] Add "Username Login" link

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -3,10 +3,10 @@ import Container from '@mui/material/Container';
 import LinkIcon from '@mui/icons-material/Link';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
-import { dropdownsEndpoint, loginEndpoint, makeRequest, oauthEndpoint } from '../Services/requests';
+import {dropdownsEndpoint, loginEndpoint, makeRequest, oauthEndpoint} from '../Services/requests';
 import logo from '../Images/logo.png';
 import UseFetchData from '../Hooks/UseFetchData';
 import DropdownsContext from '../Context/DropdownsContext';
@@ -32,41 +32,51 @@ const StyledForm = styled.form`
 `;
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
-  const {setDropdowns} = useContext(DropdownsContext);
-  const fetchDropdowns = UseFetchData(dropdownsEndpoint, setDropdowns);
+    const navigate = useNavigate();
+    const {setDropdowns} = useContext(DropdownsContext);
+    const fetchDropdowns = UseFetchData(dropdownsEndpoint, setDropdowns);
 
-  const login = async (values: Object) => {
-    await makeRequest(loginEndpoint, "POST", values);
-    await fetchDropdowns();
-    navigate("/");
-  };
+    const login = async (values: Object) => {
+        await makeRequest(loginEndpoint, "POST", values);
+        await fetchDropdowns();
+        navigate("/");
+    };
 
-  return (
-    <Container maxWidth="sm">
-      <StyledImageContainer>
-        <Typography
-          variant="h3"
-          component="h2"
-          gutterBottom
-          color="textPrimary"
-        >
-          Ma petite permanence
-        </Typography>
-        <StyledImage src={logo} alt="logo"/>
-      </StyledImageContainer>
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        style={{marginTop: 50, marginBottom: 100, fontSize: 20, width: '100%', minHeight: '120px'}}
-        href={oauthEndpoint}
-      >
-        <LinkIcon style={{marginRight: 20}}/>
-        Me connecter avec Reconnect Pro
-      </Button>
-    </Container>
-  );
+    return (
+        <Container maxWidth="sm">
+            <StyledImageContainer>
+                <Typography
+                    variant="h3"
+                    component="h2"
+                    gutterBottom
+                    color="textPrimary"
+                >
+                    Ma petite permanence
+                </Typography>
+                <StyledImage src={logo} alt="logo"/>
+            </StyledImageContainer>
+            <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                style={{marginTop: 50, marginBottom: 100, fontSize: 20, width: '100%', minHeight: '120px'}}
+                href={oauthEndpoint}
+            >
+                <LinkIcon style={{marginRight: 20}}/>
+                Me connecter avec Reconnect Pro
+            </Button>
+            <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                style={{marginTop: 50, marginBottom: 100, fontSize: 20, width: '100%', minHeight: '120px'}}
+                href={loginEndpoint}
+            >
+                <LinkIcon style={{marginRight: 20}}/>
+                Me connecter avec username et password
+            </Button>
+        </Container>
+    );
 };
 
 export default Login;
