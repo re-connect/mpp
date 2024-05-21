51e2203402307d53e6194d11d264965273f9c1e8
AntoineAresu <aresu.antoine@gmail.com>
2024-05-21 15:06:40 +0200

    :rocket: [Deploy] Reset opcache on deploy

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/symfony.php';
 // Config
 
 set('repository', 'git@github.com:re-connect/mpp.git');
-set('branch', 'master');
+set('branch', 'prod');
 set('symfony_env', 'prod');
 set('env', [
     'SYMFONY_ENV' => get('symfony_env'),
@@ -28,7 +28,10 @@ add('writable_dirs', []);
 
 host('155.133.130.39')
     ->set('remote_user', 'www-data')
-    ->set('deploy_path', '~/mpp');
+    ->set('deploy_path', '~/mpp')
+    ->set('flush_cache_file_name', 'flush-cache.php')
+    ->set('flush_cache_file_path', '{{current_path}}/public/{{flush_cache_file_name}}')
+    ->set('api_url', 'https://api.mpp.reconnect.fr');
 
 // Tasks
 
@@ -38,9 +41,17 @@ task('deploy:install_frontend', function () {
 task('deploy:build_frontend', function () {
     run('cd {{release_path}}/client && npm run build', ['timeout' => null]);
 });
+task('deploy:reset-opcache', function () {
+    run('sleep 5');
+    run('echo "<?php opcache_reset(); ?>" >> {{flush_cache_file_path}}');
+    run('sleep 5');
+    run('wget "{{api_url}}/{{flush_cache_file_name}}" --spider --retry-connrefused -t 5');
+    run('rm {{flush_cache_file_path}}');
+});
 
 before('deploy:build_frontend', 'deploy:install_frontend');
 before('deploy:cache:clear', 'deploy:build_frontend');
-before('deploy:symlink', 'database:migrate');
+before('deploy:symlink', 'deploy:reset-opcache');
+after('deploy:reset-opcache', 'database:migrate');
 
 after('deploy:failed', 'deploy:unlock');
