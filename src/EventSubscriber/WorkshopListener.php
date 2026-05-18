97f3c63df0679fda5ae8ea525884ced40230a698
Ashad Khan <ashad.khan@reconnect.fr>
2026-05-18 15:27:06 +0200

    ♻️ [Refacto][Workshop] Move prePersist method from ActivitySubscriber to WorkshopListener

--- src/EventSubscriber/WorkshopListener.php
+++ src/EventSubscriber/WorkshopListener.php
@@ -2,22 +2,24 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
 use App\Entity\Workshop;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Workshop::class)]
 #[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: Workshop::class)]
 readonly class WorkshopListener
 {
     public function __construct(
+        private Security $security,
         private LoggerInterface $logger,
     ) {
     }
 
-
     /**
-     * @param Workshop $workshop
      * @return array<string, string>
      */
     private function getWorkshopLogData(Workshop $workshop): array
@@ -28,6 +30,18 @@ readonly class WorkshopListener
             'date' => $workshop->getCreatedAt()->format('d/m/Y H:i:s'),
             'auteur' => $workshop->getAuthor()->getUsername(),
         ];
+    }
+
+    public function prePersist(Workshop $workshop): void
+    {
+        $user = $this->security->getUser();
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $workshop->setAuthor($user);
+
+        $this->logger->info('[Workshop][prePersist] A new Workshop will be persisted', $this->getWorkshopLogData($workshop));
     }
 
     public function postPersist(Workshop $workshop): void
